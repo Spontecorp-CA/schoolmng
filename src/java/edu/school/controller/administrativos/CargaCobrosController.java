063a0c33561500f6d786b6f9083a9e1b24991270
JGCastillo <jgcastillop@gmail.com>
2016-09-16 20:21:24 -0400

    Continua carga de cobros

--- src/java/edu/school/controller/administrativos/CargaCobrosController.java
+++ src/java/edu/school/controller/administrativos/CargaCobrosController.java
@@ -22,7 +22,9 @@ public class CargaCobrosController implements Serializable{
     @Inject 
     private Pago pago;
     
-    private String monto;
+    private Double monto;
+    private boolean repeat;
+    private Integer meses;
 
     public Pago getPago() {
         return pago;
@@ -32,35 +34,44 @@ public class CargaCobrosController implements Serializable{
         this.pago = pago;
     }
 
-    public String getMonto() {
+    public Double getMonto() {
         return monto;
     }
 
-    public void setMonto(String monto) {
+    public void setMonto(Double monto) {
         this.monto = monto;
+    }
+
+    public boolean isRepeat() {
+        return repeat;
+    }
+
+    public void setRepeat(boolean repeat) {
+        this.repeat = repeat;
+    }
+
+    public Integer getMeses() {
+        return meses;
+    }
+
+    public void setMeses(Integer meses) {
+        this.meses = meses;
     }
     
     public void createPago(){
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         StringBuilder sb = new StringBuilder();
-        try {
-            double montoBs = Double.parseDouble(monto);
-            pago.setMonto(montoBs);
-        } catch (NumberFormatException e) {
-            FacesContext.getCurrentInstance().addMessage(
-                    null, 
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                            e.getMessage(),
-                            "debe ingresar un número en al campo 'Monto'"));
-            return;
-        }
-        
-        pagoFacade.create(pago);
+        pago.setMonto(monto);
+        //pagoFacade.create(pago);
          
         sb.append("El cobro a crear es  ").append(pago.getMotivo());
         sb.append(" por un monto de ").append(pago.getMonto());
         sb.append(" en la fecha de ").append(df.format(pago.getFecha()));
         sb.append(" con vencimiento ").append(df.format(pago.getFechaVencimiento()));
+        if(repeat){
+            sb.append(" y se repetirá por ").append(meses).append(" meses.");
+        }
+        
         System.out.println(sb.toString());
         
         clearFields();
@@ -73,7 +84,9 @@ public class CargaCobrosController implements Serializable{
     
     public void clearFields(){
         pago.setMotivo("");
-        monto = "";
+        monto = null;
+        meses = null;
+        repeat = false;
         pago.setFecha(null);
         pago.setFechaVencimiento(null);
     }
