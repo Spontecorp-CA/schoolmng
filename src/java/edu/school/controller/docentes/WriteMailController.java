49352a755c76a63e7f683c0c9613f43b9660fa52
José Gregorio Castillo Pacheco <jgcastillop@spontecorp.com>
2016-11-17 16:59:11 -0400

    Continua el envío de correos con adjuntos

--- src/java/edu/school/controller/docentes/WriteMailController.java
+++ src/java/edu/school/controller/docentes/WriteMailController.java
@@ -15,12 +15,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.Part;
+import org.apache.commons.io.FileUtils;
 
 @Named
 @ViewScoped
@@ -31,6 +34,8 @@ public class WriteMailController implements Serializable{
     private String subject;
     private String message;    
     private Part file;
+    private String directory;
+    private String fileLabel;
     
     @Inject
     private EmailAccount emailAccount;
@@ -42,6 +47,26 @@ public class WriteMailController implements Serializable{
     @EJB
     private EmailAccountFacadeLocal emailAccountFacade;
 
+    @PostConstruct
+    public void init(){
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        directory = context.getInitParameter("upload.location");
+        
+        Path path = Paths.get(directory);
+        try {
+          if(Files.exists(path, LinkOption.NOFOLLOW_LINKS)){
+              FileUtils.deleteDirectory(new File(directory));
+          } else {
+              Files.createDirectory(path);
+          }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(WriteMailController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        fileLabel = "  No ha seleccionado archivo";
+    }
+    
     public String getPara() {
         return para;
     }
@@ -73,6 +98,14 @@ public class WriteMailController implements Serializable{
     public void setFile(Part file) {
         this.file = file;
     }
+
+    public String getFileLabel() {
+        return fileLabel;
+    }
+
+    public void setFileLabel(String fileLabel) {
+        this.fileLabel = fileLabel;
+    }
     
     public void sendMail(){
         emailAccount = emailAccountFacade.find(1); // modificar esto de acuerdo a la cuenta que se tenga acceso
@@ -85,7 +118,7 @@ public class WriteMailController implements Serializable{
         mail.setSubject(subject);
         mail.setRecipient(para);
         
-        String filePath = "c:\\uploads\\" + file.getSubmittedFileName();
+        String filePath = directory + file.getSubmittedFileName();
         
         mail.setFilePath(filePath);
         mail.setFileName(file.getSubmittedFileName());
@@ -101,20 +134,21 @@ public class WriteMailController implements Serializable{
                         .log(Level.SEVERE, "No pudo borrar el temporal", ex);
             }
         } else {
-            JsfUtils.messageSuccess("Ha ocurrido un problema el correo no se ha podido enviar");
+            JsfUtils.messageError("Ha ocurrido un problema el correo no se ha podido enviar");
         }
        
     }
     
     public void uploadedFile(){
         try (InputStream input = file.getInputStream()){
-            Path path = Paths.get("c:\\uploads");
+            Path path = Paths.get(directory);
             if(Files.notExists(path, LinkOption.NOFOLLOW_LINKS)){
                 Files.createDirectory(path);
             }
             
             Files.copy(input, new File(path.toUri().getPath(), 
                     file.getSubmittedFileName()).toPath());
+            fileLabel = file.getSubmittedFileName();
             
         } catch (IOException e) {
             Logger.getLogger(WriteMailController.class.getName())
@@ -127,6 +161,7 @@ public class WriteMailController implements Serializable{
         this.setSubject(null);
         this.setMessage(null);
         this.file = null;
+        this.fileLabel = null;
     }
     
 }
